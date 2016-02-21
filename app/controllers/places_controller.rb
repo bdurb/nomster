8e2ffd47178b12c013df3d9e03808547b0abb3e1
Brian Durbin <brian@rpmgreenville.com>
2016-02-21 23:54:05 +0000

    Connected places with users

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,4 +1,5 @@
 class PlacesController < ApplicationController
+	before_action :authenticate_user!, only: [:new, :create]
 	def index
 		@places = Place.all.page(params[:page]).per(3)
 	end
@@ -8,7 +9,7 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		Place.create(place_params)
+		current_user.places.create(place_params)
 		redirect_to root_path
 	end
 
