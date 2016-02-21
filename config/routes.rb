828f81e1c86e4ac84652b1882994cd13f701dfa3
Brian Durbin <brian@rpmgreenville.com>
2016-02-21 01:20:52 +0000

    created first page

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Nomster::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'places#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
