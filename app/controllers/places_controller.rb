c5a809b1c10d036115e86b280b1fc68e6cca5a8d
Brian Durbin <brian@rpmgreenville.com>
2016-02-22 03:29:09 +0000

    Restricted destroy to users who created place

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,5 @@
 class PlacesController < ApplicationController
-	before_action :authenticate_user!, only: [:new, :create, :edit, :update]
+	before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
 	def index
 		@places = Place.all.page(params[:page]).per(3)
 	end
@@ -36,6 +36,10 @@ class PlacesController < ApplicationController
 
 	def destroy
 		@place = Place.find(params[:id])
+		if @place.user != current_user
+			return render text: 'Not Allowed', status: :forbidden
+		end
+	
 		@place.destroy
 		redirect_to root_path
 	end
