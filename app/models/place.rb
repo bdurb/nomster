341652638a22f3f8042896f0e8c5b2c8cc24fda1
Brian Durbin <brian@rpmgreenville.com>
2016-02-22 17:59:03 +0000

    Added additional validations

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,4 +1,7 @@
 class Place < ActiveRecord::Base
 	belongs_to :user
 	validates :name, presence: true
+	validates :name, length: {minimum: 3}
+	validates :description, presence: true
+	validates :address, presence: true
 end
