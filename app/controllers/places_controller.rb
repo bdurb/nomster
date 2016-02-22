a567b28257efb944a5924434a77121a304cfb8d0
Brian Durbin <brian@rpmgreenville.com>
2016-02-22 16:47:52 +0000

    Added validations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -9,8 +9,8 @@ class PlacesController < ApplicationController
 	end
 
 	def create
-		current_user.places.create(place_params)
-		if @place.valid
+		@place = current_user.places.create(place_params)
+		if @place.valid?
 		redirect_to root_path
 	else
 		render :new, status: :unprocessable_entity
