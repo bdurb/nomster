7d27c89d88320726e57f8f9f3ecb05b6b7cf5537
Brian Durbin <brian@rpmgreenville.com>
2016-02-22 16:25:59 +0000

    Added validations

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,7 +10,12 @@ class PlacesController < ApplicationController
 
 	def create
 		current_user.places.create(place_params)
+		if @place.valid
 		redirect_to root_path
+	else
+		render :new, status: :unprocessable_entity
+	end
+
 	end
 
 	def show
@@ -31,7 +36,11 @@ class PlacesController < ApplicationController
 			return render text: 'Not Allowed', status: :forbidden
 		end
 		@place.update_attributes(place_params)
-		redirect_to root_path
+		if @place.valid?
+   			 redirect_to root_path
+  		else
+    		 render :edit, status: :unprocessable_entity
+  		end
 	end
 
 	def destroy
@@ -39,7 +48,7 @@ class PlacesController < ApplicationController
 		if @place.user != current_user
 			return render text: 'Not Allowed', status: :forbidden
 		end
-	
+
 		@place.destroy
 		redirect_to root_path
 	end
