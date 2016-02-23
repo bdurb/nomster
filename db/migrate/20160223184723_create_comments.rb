2b4587a546718a205da5788aba8636e78b02b318
Brian Durbin <brian@rpmgreenville.com>
2016-02-23 19:18:27 +0000

    Added comment form to place page

--- db/migrate/20160223184723_create_comments.rb
+++ db/migrate/20160223184723_create_comments.rb
@@ -1,12 +1,12 @@
 class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
-    	t.text :message
-    	t.string :rating
+      t.text :message
+      t.string :rating
 
-    	t.integer :user_id
-    	t.integer :place_id
-    	t.timestamps
+      t.integer :user_id
+      t.integer :place_id
+      t.timestamps
     end
 
     add_index :comments, [:user_id, :place_id]
