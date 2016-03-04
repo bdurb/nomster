22bb4f7b5d9d5e9023a4003fce7f5c7cdfe9173b
Brian Durbin <brian@rpmgreenville.com>
2016-03-04 18:31:07 +0000

    set up submit button to save photos

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Nomster::Application.routes.draw do
 
   resources :places do
     resources :comments, only: :create
+    resources :photos, only: :create
   end
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
