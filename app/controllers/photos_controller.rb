919fa29ed01a0cea1cafdbfe2fcfaa9ba74f641f
Brian Durbin <brian@rpmgreenville.com>
2016-03-04 20:35:21 +0000

    Added place to show photos on place page

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -2,7 +2,7 @@ class PhotosController < ApplicationController
 	before_action :authenticate_user!
 	def create
 		@place = Place.find(params[:place_id])
-		@place.photos.create(photo_params.merge(user: current_user))
+		@place.photos.create(photo_params)
 		redirect_to place_path(@place)
 	end
 
