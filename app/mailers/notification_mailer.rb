662b93df0e3abfc1f9df0b06a3f9b7dd99f8a072
Brian Durbin <brian@rpmgreenville.com>
2016-03-06 01:03:59 +0000

    fixed issue between modals

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,7 +1,9 @@
 class NotificationMailer < ActionMailer::Base
   default from: "no-reply@nomsterapp.com"
 
-  def comment_added
+  def comment_added(comment)
+  	@place = comment.place
+  	@place_owner = @place_owner.email,
   	mail(to: "brian@rpmgreenville.com",
          subject: "A comment has been added to your place")
   end
